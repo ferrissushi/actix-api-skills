e4f53b545f3e5e8b23b711de5a8c9d8322bbd185
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-21 09:46:09 +0300

    feat(api): URL dispatch basics

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
-use std::net::Ipv4Addr;
+use actix_web::{web, App, HttpServer, Responder};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there")
+async fn index() -> impl Responder {
+    "Hello world"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server_info = ServerInfo {
-        adress: Ipv4Addr::new(127, 0, 0, 1),
-        port: 8080,
-    };
-    let server_adress = server_info.adress;
-    let server_port = server_info.port;
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind((server_adress, server_port))?
-    .run()
-    .await
+        App::new().service(
+            web::scope("/api").route("/index.html", web::get().to(index)),
+        )
+    }).bind(("127.0.0.1", 8080))?.run().await
 }
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct ServerInfo {
-    adress: Ipv4Addr,
-    port: u16,
-}
-
