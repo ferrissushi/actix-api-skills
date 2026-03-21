e815ed4b7f487cf399cb90db7b7023a39751c0f4
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-21 10:01:49 +0300

    feat(api): App state basics

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web::{self, Data}, App, HttpServer};
 
-async fn index() -> impl Responder {
-    "Hello world"
+struct AppState {
+    app_name: String
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api").route("/index.html", web::get().to(index)),
-        )
-    }).bind(("127.0.0.1", 8080))?.run().await
+        App::new().app_data(Data::new(AppState {
+            app_name: "state_app".to_string()
+        })).service(index)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run().await
+
 }
