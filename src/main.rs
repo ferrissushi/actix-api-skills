7a4fc93bb8039b406083ee804d3c1ee6e978ba72
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-22 15:35:27 +0300

    feat(api): compose application using an application scope

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::sync::Mutex;
 
-use actix_web::Responder;
+use actix::main;
+use actix_web::{
+    get, post,
+    web::{scope, Data, Json},
+    App, HttpResponse, HttpServer, Responder,
+};
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+struct AppState {
+    users: Mutex<Vec<User>>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 struct User {
     username: String,
     password: Option<String>,
@@ -20,8 +31,7 @@ impl Default for User {
     }
 }
 
-
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 enum Sexe {
     Male,
     Female,
@@ -34,8 +44,31 @@ impl Default for Sexe {
     }
 }
 
-const USERS: Vec<User> = Vec::new();
+#[get("/users")]
+async fn get_users(data: Data<AppState>) -> impl Responder {
+    let users = data.users.lock().unwrap();
+    HttpResponse::Ok().json(&*users)
+}
 
-async fn get_users() -> impl Responder {
-    USERS
+#[post("/users")]
+async fn post_users(user: Json<User>, data: Data<AppState>) -> impl Responder {
+    let mut users = data.users.lock().unwrap();
+    users.push(user.into_inner());
+    HttpResponse::Created().json(&*users)
+}
+
+#[main]
+async fn main() -> std::io::Result<()> {
+    let app_state = Data::new(AppState {
+        users: Mutex::new(Vec::new()),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(scope("/api").service(get_users).service(post_users)) // ← inside closure
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
