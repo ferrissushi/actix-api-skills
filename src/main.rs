b0fa45ff61ee44ae2348451f398cd5a9252c30cc
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-24 11:43:45 +0300

    feat(api): configure part for refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use actix_web::{guard, main, web, App, HttpResponse, HttpServer};
+use actix::main;
+use actix_web::{
+    web::{self, get, head, ServiceConfig},
+    App, HttpResponse, HttpServer, Responder,
+};
+
+async fn default_service() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
+}
+
+async fn not_allowed() -> impl Responder {
+    HttpResponse::MethodNotAllowed()
+}
+
+fn config(cfg: &mut ServiceConfig) {
+    cfg.service(
+        web::resource("/path")
+            .route(get().to(default_service))
+            .route(head().to(not_allowed)),
+    );
+}
 
 #[main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(
-                web::scope("/")
-                    .guard(guard::Host("www.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
-            )
-            .route("/", web::to(HttpResponse::Ok))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().configure(config))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
