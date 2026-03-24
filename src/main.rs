9a9c8b51bc0f59801131757081fe3566897c4530
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-24 13:10:19 +0300

    feat(api): add dynamic route

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,20 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello")
 }
 
+#[get("/hello/{name}")]
+async fn greeting(path: web::Path<String>) -> impl Responder {
+    let name = path.into_inner();
+    let msg = format!("Hello {}", name);
+    HttpResponse::Ok().body(msg)
+}
+
 #[main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(web::scope("/api").route("/health", web::get().to(health)))
             .service(hello)
+            .service(greeting)
     })
     .workers(4)
     .shutdown_timeout(100)
