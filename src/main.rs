6b7bb8159d77089be0cce200f7c4bb181ee0f643
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-24 12:16:06 +0300

    fix(api): add health factory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use actix_web::{main, web::get, App, HttpResponse, HttpServer};
+use actix_web::{main, web, App, HttpResponse, HttpServer, Responder};
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
+}
 
 #[main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", get().to(HttpResponse::Ok())))
+    HttpServer::new(|| App::new().route("/", web::get().to(health)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
