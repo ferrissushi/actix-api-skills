be3fcf724adb79c7f9687d58895972c64a1b6e14
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-24 14:04:11 +0300

    feat(api): get json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{main, post, web::Json, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{main, post, web::Json, App, HttpResponse, HttpServer, Responder, get};
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 struct User {
     username: String,
     password: String,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self { username: "John".to_string(), password: "        ".to_string()}
+    }
 }
 
 #[post("/users")]
@@ -16,9 +22,15 @@ async fn create_user(user: Json<User>) -> impl Responder {
     HttpResponse::Created().body(msg)
 }
 
+#[get("/users")]
+async fn get_user() -> impl Responder {
+    let user: User = Default::default();
+    HttpResponse::Ok().json(user)
+}
+
 #[main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(create_user))
+    HttpServer::new(|| App::new().service(create_user).service(get_user))
         .bind("0.0.0.0:8080")?
         .run()
         .await
