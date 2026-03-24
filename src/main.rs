4bd5e443568feaf4fa607041f9ab5aa157592dd0
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-24 12:12:27 +0300

    feat(api): basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,9 @@
-use actix::main;
-use actix_web::{
-    web::{self, get, head, ServiceConfig},
-    App, HttpResponse, HttpServer, Responder,
-};
-
-async fn default_service() -> impl Responder {
-    HttpResponse::Ok().body("Ok")
-}
-
-async fn not_allowed() -> impl Responder {
-    HttpResponse::MethodNotAllowed()
-}
-
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello")
-}
-
-fn config(cfg: &mut ServiceConfig) {
-    cfg.service(
-        web::resource("/path")
-            .route(get().to(default_service))
-            .route(head().to(not_allowed)),
-    );
-}
-
-fn scoped_config(cfg: &mut ServiceConfig) {
-    cfg.service(web::resource("/hello").route(get().to(hello)));
-}
+use actix_web::{main, web::get, App, HttpResponse, HttpServer};
 
 #[main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .configure(config)
-            .service(web::scope("/api").configure(scoped_config))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", get().to(HttpResponse::Ok())))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
